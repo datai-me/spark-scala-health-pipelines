6157031b35ff0062eeabba9e0626024c0e0e4af9
RANOELISON Dimbisoa Patrick <ranoelisondimbisoapatrick@gmail.com>
2026-02-05 13:08:48 +0100

    add hadoop configurations

--- src/main/scala/pipeline/Main.scala
+++ src/main/scala/pipeline/Main.scala
@@ -12,6 +12,8 @@ import config.ConfigLoader
 object Main {
 
   def main(args: Array[String]): Unit = {
+    System.setProperty("hadoop.home.dir", "C:\\hadoop\\")
+    // System.setProperty("spark.hadoop.io.native.lib.available", "false")
 
     val spark = SparkSession.builder()
       .appName(ConfigLoader.appName)
